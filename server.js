a7722b917c36cc910c5858a0bb9a7601c42dd8c8
Deena Smith <deenasmith1630@gmail.com>
2022-04-21 09:36:48 -0500

    server established, express added

---
+++ server.js
@@ -0,0 +1,24 @@
+
+//Requirements
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+
+
+// Server
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
