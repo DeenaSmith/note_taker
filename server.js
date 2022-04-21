255b0d6b525b74dfce73737494c85575578b618e
Deena Smith <deenasmith1630@gmail.com>
2022-04-21 10:54:29 -0500

    get and post routes from database

--- server.js
+++ server.js
@@ -16,6 +16,35 @@ app.use(express.static('public'));
 
 
 
+// Routes/Requests
+app.get("api/notes", function(req, res) {
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    })
+});
+
+
+
+app.post("/api/notes", function(req, res) {
+    const note = req.body;
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        note.id = notes.lenth + 1;
+        notes.push(note);
+    }).then(function(notes) {
+        writeFileAsynch("./develop/db/db.json", JSON.stringify(notes));
+        res.json(note);
+    })
+
+});
+
+
+
+// Routes that push to HTML pages
+app.get("/notes", function(req, res) {
+    
+})
 
 
 
